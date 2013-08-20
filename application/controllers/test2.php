3e742ee2a1d1a1c1e279621a483ad4ac4d484ff6
azerozvn <nguoichet001@yahoo.com>
2013-08-20 21:40:31 +0700

    Test git add

---
+++ application/controllers/test2.php
@@ -0,0 +1,6 @@
+<?php 
+	class Test2 extends CI_Controller{
+		public function index(){
+			echo 'something balh blah blah';
+		}
+	}
---
+++ application/controllers/test3.php
@@ -0,0 +1,6 @@
+<?php
+	class Test3 extends CI_Controller{
+		public function index(){
+			echo 'day la cai test 3 ne';
+		}
+	}
