0613b1dde47820bd928887f0312ede5c76488328
azerozvn <nguoichet001@yahoo.com>
2013-08-20 21:32:45 +0700

    Just test

---
+++ application/controllers/test.php
@@ -0,0 +1,11 @@
+<?php
+
+class Test extends CI_Controller {
+	public function wtf($name, $age)
+	{
+		echo 'Hello World ' . $name . $age;
+	}
+	public function index(){
+		echo 'cai gi gi do';
+	}
+}
