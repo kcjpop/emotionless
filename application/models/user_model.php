773cfc39dc27132cfcabf3d1350db4009315374b
kcjpop <kcj.pop.com@gmail.com>
2013-09-04 15:29:59 +0300

    Add method to get/update/delete users.

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -1,12 +1,18 @@
 <?php 
+/**
+ * User Model
+ *
+ * @author An Cao <ping@ancaois.me>
+ */
 class User_model extends CI_Model
 {
 	protected $table = 'users';
 
 	public function __construct()
 	{
-		// Database is already loaded in all model classes
 		parent::__construct();
+		// Load the database
+		$this->load->database();
 	}
 
 	/**
@@ -18,5 +24,54 @@ class User_model extends CI_Model
 	{
 		// Simple, huh?
 		$this->db->insert($this->table, $data);
+	}
+
+	/**
+	 * Get a user by his/her ID
+	 *
+	 * @param  int $id 
+	 *
+	 * @return array
+	 */
+	public function get($id)
+	{
+		$this->db->where('id', (int) $id);
+		return $this->db->get($this->table)->row_array();
+	}
+
+	/**
+	 * Get all users in the database
+	 *
+	 * @return array
+	 */
+	public function gets()
+	{
+		return $this->db->get($this->table)->result_array();
+	}
+
+	/**
+	 * Update a user information
+	 *
+	 * @param  array $data 
+	 *
+	 * @return void
+	 */
+	public function update($data)
+	{
+		$this->db->where('id', (int) $data['id']);
+		return $this->db->update($this->table, $data);
+	}
+
+	/**
+	 * Delete a user
+	 *
+	 * @param  int $id 
+	 *
+	 * @return void
+	 */
+	public function delete($id)
+	{
+		$this->db->where('id', (int) $id);
+		return $this->db->delete($this->table);
 	}
 }
