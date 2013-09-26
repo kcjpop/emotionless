74e19ca85737118a0887accef6419a91f87db0ab
kcjpop <kcj.pop.com@gmail.com>
2013-09-26 11:03:50 +0300

    Add more TinyMCE4 plugins.

--- application/views/base/admin_footer.php
+++ application/views/base/admin_footer.php
@@ -3,7 +3,21 @@
 <script src="//tinymce.cachefly.net/4.0/tinymce.min.js"></script>
 <script>
 $(function() {
-	tinymce.init({selector:'textarea.rich'});
+	tinymce.PluginManager.load('moxiemanager', '//www.tinymce.com/js/moxiemanager/plugin.min.js');
+	tinymce.init({
+		selector:'textarea.rich',
+		theme: "modern",
+		plugins: [
+			"advlist autolink lists link image charmap print preview hr anchor pagebreak",
+			"searchreplace wordcount visualblocks visualchars code fullscreen",
+			"insertdatetime media nonbreaking save table contextmenu directionality",
+			"emoticons template paste textcolor moxiemanager"
+		],
+		toolbar1: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
+		toolbar2: "print preview media | forecolor backcolor emoticons",
+		image_advtab: true,
+		autosave_ask_before_unload: false
+	});
 });
 </script>
 </body>
